7360ff2306ec9c27294b3d75d0494feceadcdab6
attoumbre <juvenalak@gmail.com>
2023-08-27 18:27:20 +0200

    add admin header and nav bar

---
+++ src/components/AdminHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="id_header">
+    Header Admin
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminHeader',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.id_header{
+  border-bottom: solid 1px black;
+}
+</style>
---
+++ src/components/AdminNav.vue
@@ -0,0 +1,18 @@
+<template >
+    <div>
+        <ul>
+            <li > item</li>
+            <li > item</li>
+            <li > item</li>
+        </ul>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:"AdminNav"
+}
+</script>
+<style >
+    
+</style>
